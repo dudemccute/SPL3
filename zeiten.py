d2a56c78c34d008fc5d71e72f40adf297a8f843a
dudemccute <171witb15@o365.htl-leoben.at>
2018-04-25 10:42:12 +0200

    zeiten.py

--- zeiten.py
+++ zeiten.py
@@ -1,1 +1,27 @@
-# zeiten.py 
+# zeiten.py
+ 
+def zeitInSekunden(h,m,s)
+    gesamt = 0
+    gesamt += h*3600
+    gesamt += m*60
+    gesamt += s 
+    return gesamt
+
+beginnZeit = input("Beginnzeit: ")
+endZeit = input("Endezeit: ")
+#endeZeit = input("Endezeit: ")
+
+#Zeit im Format HH:MM:SS
+beginn = beginnZeit.split(":")
+print(beginn)
+
+h = int(beginn[0])
+m = int(beginn[1])
+s = int(beginn[2])
+
+print("Stunden",h)
+print("Minuten",m)
+print("Sekunden",s)
+
+beginnSekunden = s + m*60 + h*3600
+print("Gesamt Sekunden Beginn:",beginnSekunden)
