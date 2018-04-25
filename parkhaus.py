d827a721fd6666bb74ba3f3c420ed139f3160f01
dudemccute <171witb15@o365.htl-leoben.at>
2018-04-25 09:12:47 +0200

    Haltstellen

--- parkhaus.py
+++ parkhaus.py
@@ -1,2 +1,11 @@
 # parkhaus.py 
 # Angabe für das Beispiel: siehe Moodle
+
+print("Linienbus-Simu
+
+Haltestelle = input("Wie viele Haltestellen gibt es ?")
+
+ = int(einsteiger)
+
+for h in range (0,10):
+    print("Guten Morgen!")
