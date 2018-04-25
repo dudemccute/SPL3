077ad3a7a6ee595de30b4d0c3f7e2508a72f5b1b
dudemccute <171witb15@o365.htl-leoben.at>
2018-04-25 10:29:02 +0200

    parkhaus.py

--- parkhaus.py
+++ parkhaus.py
@@ -1,11 +1,42 @@
 # parkhaus.py 
 # Angabe für das Beispiel: siehe Moodle
 
-print("Linienbus-Simu
+p = 0
+print("Linienbus-Simulator")
 
-Haltestelle = input("Wie viele Haltestellen gibt es ?")
+Haltestellen = input("Wie viele Haltestellen gibt es ? ")
 
- = int(einsteiger)
+Haltestellen = int(Haltestellen)
+i = 0
 
-for h in range (0,10):
-    print("Guten Morgen!")
+while (i < Haltestellen):
+    i = i+1
+    print("Sie sind an der:",i,".Haltstelle,Wie viele Personen steigen ein ")
+    einsteiger = input()
+    inside =int (einsteiger)
+
+    print("Wv Austeiger?")
+    Austeiger = input()
+    out = int(Austeiger)
+
+    p = p + inside
+    p = p - out
+     
+    if(p > 60):
+        print("Es sind zu viele im Bus ")
+        r = p - 60
+        print("Es sind ",r,"Personen zu viel ")
+        p = 60
+        print("Es sind nun",p,"Menschen im Bus")
+    elif(p < 0):
+        print("Es sind zu wenig im Bus ")
+        i = i-1
+        p = p-inside
+        p = p+out
+        
+    else:
+        print("Es sind nun",p,"Menschen im Bus ")
+    
+
+
+
